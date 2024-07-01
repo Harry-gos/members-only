e24da38b0b4be7bd8684f74901e5cb2092261cfb
Hung Pham <harry.pham.go@gmail.com>
2024-07-02 01:19:12 +0700

    Use Devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,30 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @post = Post.new
   end
 
   def create
+    post_hash = post_params
+    post_hash[:user_id] = current_user.id
+  
+    @post = Post.new(post_hash)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:link)
   end
 end
